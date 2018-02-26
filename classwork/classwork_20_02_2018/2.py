b565cb88e63b5e894967fdef2de15b032d7ef72d
Qvazi141 <ybulakhau@gmail.com>
2018-02-26 22:19:57 +0300

    Fix classwork_20_02_2018/2.py, made the upper register

--- classwork/classwork_20_02_2018/2.py
+++ classwork/classwork_20_02_2018/2.py
@@ -1,7 +1,7 @@
 # Дана строка ‘Hello!Anthony!Have!A!Good!Day!’.
 # Создать список, состоящий из отдельных слов [‘HELLO’, ‘ANTHONY’, ‘HAVE’, ‘A’, ‘GOOD’, ‘DAY’].
 s1 = 'Hello!Anthony!Have!A!Good!Day!'
-s1 = s1.split('!')
+s1 = s1.upper().split('!')
 if s1[-1] == '':
     s1 = s1[:-1]
 print(s1)
