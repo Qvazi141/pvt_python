789c410a6d308d73531b8fa41d0a41cab0b9f81d
Qvazi141 <ybulakhau@gmail.com>
2018-02-27 23:25:41 +0300

    classwork 27_02_2018, task 0,4

--- classwork/classwork_20_02_2018/4.py
+++ classwork/classwork_20_02_2018/4.py
@@ -2,7 +2,7 @@
 # Необходимо определить и вывести пол человека, которому данное письмо адресовано.
 # Приглашение письма запрашивается у пользователя.
 message = input()
-if 'Mr.' in message or 'Mrs.' in message:
+if 'Mr.' in message:
     print('Male')
-elif 'Miss.' in message or 'Ms.' in message:
+elif 'Miss.' in message or 'Ms.' in message or 'Mrs.' in message:
     print('Female')
