8d2f9199dc4ff3046cf87690a41b7d247879ee40
Eugene Bulakhau <ybulakhau@gmail.com>
2018-03-01 16:08:24 +0300

    fix 4.py

--- classwork/classwork_27_02_2018/4.py
+++ classwork/classwork_27_02_2018/4.py
@@ -4,14 +4,14 @@
 
 # функция инициализация двухмерного списка
 def initialization(dimension):
-    return [[int(input('Input elemet {0} {1}: '.format(i, j))) for j in range(dimension)] for i in range(dimension)]
+    return [[int(input('Input element {0} {1}: '.format(i, j))) for j in range(dimension)] for i in range(dimension)]
 
 
 # функция подсчета положительных элементов, выше главной диагонали
 def positive_count(matrix):
     count = 0
-    for i in range(n):
-        for j in range(n):
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
             if j > i and matrix[i][j] > 0:
                 count += 1
     return count
@@ -29,4 +29,4 @@ if __name__ == '__main__':
     n = int(input('Input N: '))
     a = initialization(n)
     print_matrix(a)
-    print('The number of positive elements %d' % positive_count(a))
+    print('The number of positive elements: %d' % positive_count(a))
