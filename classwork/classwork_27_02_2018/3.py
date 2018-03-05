23a903b6a02d3adcf277bb0120a7f7d0a09e128b
Qvazi141 <ybulakhau@gmail.com>
2018-03-05 23:24:10 +0300

    class work 27_02_2018 task 3

--- classwork/classwork_27_02_2018/3.py
+++ classwork/classwork_27_02_2018/3.py
@@ -1,18 +1,23 @@
 # Реализовать программу с базой учащихся группы. Записи по учащемуся: имя, фамилия, пол, возраст.
 # Программа должна предусматривать поиск по одному или нескольким полям базы.
 # Результат выводить в удобочитаемом виде с порядковым номером записи.
-const_list = ['Name', 'Surname', 'Sex', 'Age']
 
 
-def add():
+def add_student():
+    const_list = ['Name', 'Surname', 'Sex', 'Age']
     student = {}
     for i in const_list:
         student[i] = input('Input {} : '.format(i))
     return student
 
 
-def find(fild):
-    return student
+def del_student(student_bd, i):
+    del student_bd[i]
+    return student_bd
+
+
+#def find(fild):
+#    return student
 
 
 def print_bd(student_bd):
@@ -26,14 +31,15 @@ def print_bd(student_bd):
 if __name__ == '__main__':
     bd = {0:{'Name':'Lera', 'Surname':'Scomor', 'Sex':'Female', 'Age':'22'}}
     while True:
-        inp = input('Enter "add" - for add student\n"print" - for print group list\n"find" - for find\n'
-                    '"q" - for exit program \n')
+        inp = input('Enter "add" - for add student\n"del" - for delete student\n"print" - for print group list'
+                    '\n"find" - for find\n"q" - for exit program \n')
         if inp == 'q':
             break
         elif inp == 'add':
-            bd[len(bd)] = add()
+            bd[len(bd)] = add_student()
+        elif inp == 'del':
+            bd = del_student(bd, int(input('input student number: ')))
         elif inp == 'find':
             f = input('Enter search fields Name/Surname/Sex/Age: ')
-
         elif inp == 'print':
             print_bd(bd)
