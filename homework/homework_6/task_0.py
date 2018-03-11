d103ce829fb2d6c9b424a37fdc54b1e2b965c350
Qvazi141 <ybulakhau@gmail.com>
2018-03-11 23:19:00 +0300

    homework 6 - add test func for task 0

--- homework/homework_6/task_0.py
+++ homework/homework_6/task_0.py
@@ -3,12 +3,21 @@
 # Для каждой из пользовательских функций написать функцию-тест.
 
 
+# функция добавления длинны слова - в начало слова
 def find_len(string, separator):
     list_for_work = string.split(separator)
     return list(map(lambda x: str(len(x))+x, list_for_work))
+
+
+# функция для тестирования функции find_len
+def test_find_len(test_string='hello!test', test_separator='!', result=['5hello', '4test']):
+    test = find_len(test_string, test_separator)
+    if test == result:
+        print('test passed')
 
 
 if __name__ == '__main__':
     my_string = input('Enter string: ')
     my_separator = input('Enter separator: ')
     print(find_len(my_string, my_separator))
+    test_find_len()
