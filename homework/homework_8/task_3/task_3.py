5c359cd1bab4495aba22a49c5f4efac3c55a20f0
Qvazi141 <ybulakhau@gmail.com>
2018-03-21 21:59:30 +0300

    homework_8 - add task3, add func add, del

--- homework/homework_8/task_3/task_3.py
+++ homework/homework_8/task_3/task_3.py
@@ -7,35 +7,50 @@
 def print_menu():
     print("-"*40)
     print("Main menu:")
-    print("\tmenu n\t - for print main menu")
-    print("\tadd n\t - for add item")
-    print("\tdel n\t - for fr del item")
-    print("\tfind n\t - for find word")
+    print("\tmenu - for print main menu")
+    print("\tadd - for add item")
+    print("\tdel - for fr del item")
+    print("\tfind - for find word")
     print("-" * 40)
 
 
-def menu_selection(items):
+def menu_selection(items, path):
     cursor = input(">>> ").lower()
     if cursor == "menu":
         items["menu"]()
     elif cursor == "add":
-        items["add"]()
+        with open(path, "a", encoding='utf-8') as f:
+            f.write(" ".join(items["add"]())+'\n')
     elif cursor == "del":
-        items["remove"]()
+        items["del"]()
     elif cursor == "find":
         items["find"]()
 
 
 def add():
-    pass
+    bd_struct = ("Name", "Surname", "Sex", "Age")
+    add_list = []
+    for i in bd_struct:
+        add_list.append(input("Enter {}: ".format(i)))
+    return add_list
+
 
 def remove():
-    pass
+    n = int(input("Enter element №: "))
+    with open(path, "r+", encoding='utf-8') as f:
+        temp = f.readlines()
+        f.seek(0)
+        for line in range(len(temp)):
+             if line+1 != n:
+                 f.write(temp[line])
+        f.truncate()
+        
 
 def find():
     pass
 
 if __name__ == "__main__":
+    path = "student_bd.txt"
     q = True
     menu_items = {
         "menu": print_menu,
@@ -45,4 +60,4 @@ if __name__ == "__main__":
     }
     print_menu()
     while q:
-        menu_selection(menu_items)
+        menu_selection(menu_items, path)
