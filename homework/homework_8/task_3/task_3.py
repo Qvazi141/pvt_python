89e1f4e1260ec8c6733c9e80ffb85a2c6cf18382
Eugene Bulakhau <ybulakhau@gmail.com>
2018-03-22 14:15:37 +0300

    issues #9 fix - add search by several fields.

--- homework/homework_8/task_3/task_3.py
+++ homework/homework_8/task_3/task_3.py
@@ -5,16 +5,19 @@
 
 
 def print_menu():
+    """Функция вывода основного меню."""
     print("-"*40)
     print("Main menu:")
     print("\tmenu - for print main menu")
     print("\tadd - for add item")
     print("\tdel - for fr del item")
     print("\tfind - for find word")
+    print("\tq - for quit program")
     print("-" * 40)
 
 
 def menu_selection(items, path):
+    """Функция выбора меню. Осуществляет вызов других функций."""
     cursor = input(">>> ").lower()
     if cursor == "menu":
         items["menu"]()
@@ -24,40 +27,75 @@ def menu_selection(items, path):
     elif cursor == "del":
         items["del"]()
     elif cursor == "find":
-        items["find"]()
+        with open(path, "r", encoding='utf-8') as f:
+            bd_for_search = f.readlines()
+        print(items["find"](bd_for_search))
+    elif cursor == "q":
+        return items["quit"]()
+    return True
 
 
 def add():
-    bd_struct = ("Name", "Surname", "Sex", "Age")
+    """Функция добавления элемента. Возвращает список для добавления."""
+    bd_structure = ("Name", "Surname", "Sex", "Age")
     add_list = []
-    for i in bd_struct:
+    for i in bd_structure:
         add_list.append(input("Enter {}: ".format(i)))
     return add_list
 
 
 def remove():
+    """Функция удаления элементов. Удаляет по номеру, возвращает None"""
     n = int(input("Enter element №: "))
     with open(path, "r+", encoding='utf-8') as f:
         temp = f.readlines()
         f.seek(0)
         for line in range(len(temp)):
-             if line+1 != n:
-                 f.write(temp[line])
+            if not line + 1 == n:
+                f.write(temp[line])
         f.truncate()
         
 
-def find():
-    pass
+def find(bd_in_list):
+    """Функция поиска. Возвращает словарь совпадений"""
+    # подготовка структуры для поиска
+    bd_structure = ("Name", "Surname", "Sex", "Age")
+    bd_in_dict = {}
+    try:
+        for element_id, element in enumerate(bd_in_list):
+                temp = element.split()
+                bd_in_dict[element_id] = {bd_structure[i]: temp[i].lower() for i in range(len(bd_structure))}
+        bd_in_list.clear()
+        temp.clear()
+    except:
+        return "Database is empty" if bd_in_list == [] else "Database structure error"
+    # ввод данных для поиска
+    fields = input('Enter search fields Name/Surname/Sex/Age: ').split("/")
+    key_words = {field: input("{}: ".format(field)).lower() for field in fields}
+    result = {}
+    # поиск
+    for element_id, element in enumerate(bd_in_dict):
+        for field in fields:
+            if bd_in_dict[element].get(field) == key_words.get(field):
+                result[element_id] = bd_in_dict[element]
+    return result
+
+
+def exit_application():
+    """Функция закрытия программы. Возвращает значение False"""
+    return False
+
 
 if __name__ == "__main__":
     path = "student_bd.txt"
-    q = True
+    quit_program = True
     menu_items = {
         "menu": print_menu,
         "add": add,
         "del": remove,
-        "find": find
+        "find": find,
+        "quit": exit_application
     }
     print_menu()
-    while q:
-        menu_selection(menu_items, path)
+    while quit_program:
+        quit_program = menu_selection(menu_items, path)
