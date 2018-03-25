c29dc413bd41dd01024e4fbfeb7dbc00097d9bd5
Qvazi141 <ybulakhau@gmail.com>
2018-03-25 19:21:35 +0300

    home_work 8 task 3 - add test

--- homework/homework_8/task_3/task_3.py
+++ homework/homework_8/task_3/task_3.py
@@ -2,7 +2,7 @@
 # Записи по учащемуся: имя, фамилия, пол, возраст.
 # Программа должна предусматривать поиск по одному или нескольким полям базы.
 # Результат выводить в удобочитаемом виде с порядковым номером записи.
-
+from packet_for_test_hw_8 import test_task_3
 
 def print_menu():
     """Функция вывода основного меню."""
@@ -25,7 +25,7 @@ def menu_selection(items, path):
         with open(path, "a", encoding='utf-8') as f:
             f.write(" ".join(items["add"]())+'\n')
     elif cursor == "del":
-        items["del"]()
+        items["del"](path)
     elif cursor == "find":
         with open(path, "r", encoding='utf-8') as f:
             bd_for_search = f.readlines()
@@ -44,8 +44,8 @@ def add():
     return add_list
 
 
-def remove():
-    """Функция удаления элементов. Удаляет по номеру, возвращает None"""
+def remove(path):
+    """Функция удаления элементов. Удаляет по номеру, возвращает номер удаленного элемента"""
     n = int(input("Enter element №: "))
     with open(path, "r+", encoding='utf-8') as f:
         temp = f.readlines()
@@ -54,6 +54,7 @@ def remove():
             if not line + 1 == n:
                 f.write(temp[line])
         f.truncate()
+    return n
         
 
 def find(bd_in_list):
@@ -87,6 +88,9 @@ def exit_application():
 
 
 if __name__ == "__main__":
+    # test_task_3.test_add(add)
+    # test_task_3.test_remove(remove)
+    # test_task_3.test_find(find)
     path = "student_bd.txt"
     quit_program = True
     menu_items = {
