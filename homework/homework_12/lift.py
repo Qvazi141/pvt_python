b614bc63f591367001eb912ce8e5147f4cb15fca
Qvazi141 <ybulakhau@gmail.com>
2018-04-01 23:20:31 +0300

    homework 12 lift - fix main menu

--- homework/homework_12/lift.py
+++ homework/homework_12/lift.py
@@ -1,9 +1,18 @@
+# Реализовать класс лифта Elevator. Класс должен обладать методом, lift, отвечающий за вызов лифта.
+# При сложении/вычитании экземпляров класса должно возвращаться значение производимой математической операции.
+# Если производить вычитание у лифта,
+# который еще не совершал поднятий, должна быть выведена ошибка неправильной операции.
+# Предусмотреть возможность сравнения какой из лифтов совершил большее количество поднятий.
+# Также необходимо предусмотреть подсчет общего количества поднятий всех лифтов.
+# При строчных операциях необходимо вывести детальную информацию по лифту:
+# наименование, количество поднятий и процент от общего количества поднятий всех лифтов.
+
+
 class Elevator:
     total = 0
 
     def __init__(self, name):
         self.name = name
-        self.count_up = 0
         self.floor = 0
 
     def lift(self):
@@ -22,24 +31,37 @@ class Elevator:
 
     def commands(self):
         print("Commands:")
+        print("help - for print commands")
         print("lift - for lift")
         print("[lift1] - [lift2] - for sub lift")
-
+        print("q - for quit elevator")
+        print("exit - for exit program")
 
 
 if __name__ == "__main__":
-    app_close = True
-    count = int(input("Enter lift number: "))
-    elevators = {}
-    commands = {
-        "lift":
-        "total":
-        ""
-    }
+    app_close = False
+    count = int(input("Enter the number of elevators: "))
+    elevators = []
     for i in range(count):
-        elevators[i+1] = Elevator()
-    while app_close:
-        lift = input("Enter lift for do: ")
-        lift_exit = True
-            while lift_exit:
+        elevators.append(Elevator(input("Enter elevator {} name: ".format(i+1))))
+    while not app_close:
+        print("-"*20)
+        print("Total number of lifts: {}".format(Elevator.total))
+        for i in range(count):
+            print("Elevator {0}: name - {1}, number of lifts - {2}".format(i+1, elevators[i].name, elevators[i].floor))
+        elevator = int(input("Enter lift for do: ")) - 1
+        elevator_exit = False
+        elevators[elevator].commands()
+        while not elevator_exit:
+            command = input(">>> ")
+            if command == "help":
+                elevators[elevator].commands()
+            elif command == "lift":
+                elevators[elevator].lift()
+            elif command == "q":
+                elevator_exit = True
+            elif command == "exit":
+                elevator_exit = True
+                app_close = True
+
 
