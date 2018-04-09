fdf578da148575bb0e2575598c0a22b8638366fa
Eugene Bulakhau <ybulakhau@gmail.com>
2018-04-09 20:07:54 +0300

    homework 13 - add load data from json, create object for each data.

--- homework/homework_13/football.py
+++ homework/homework_13/football.py
@@ -3,45 +3,71 @@ import json
 
 
 class BD:
-    def __init__(self, bd):
-        self.bd_name = bd
-        self.bd_in_dict = {}
+    def __init__(self, bd, name):
+        self._bd_path = bd
+        self._bd_name = name
+        self._bd_in_dict = {}
+        self.obj_in_dict = {}
 
-    def bd_initializations(self, bd_name):
-        with open(self.bd_name, 'r', encoding='utf-8') as f:
+    def bd_initializations(self):
+        with open(self._bd_path, 'r', encoding='utf-8') as f:
             try:
-                self.bd_in_dict = json.load(f)
-                for student_id in self.bd_in_dict:
+                self._bd_in_dict = json.load(f)
+                for key in self._bd_in_dict:
+                    self._create_object(key, self._bd_in_dict.get(key))
+                self._bd_in_dict.clear()
             except ValueError:
                 print("Bd structure error")
 
+    def _create_object(self, object_id, data):
+        if self._bd_name == "players":
+            self.obj_in_dict[object_id] = Player(object_id, data)
+        if self._bd_name == "teams":
+            self.obj_in_dict[object_id] = Team(object_id, data, players)
+
+
 class Team:
-    def __init__(self, name, players):
-        self.team_id = uuid.uuid4()
-        self.name = name
-        self.players = players
+    _structure_team = ['Name']
+
+    def __init__(self, team_id, data, bd_players):
+        self._team_id = team_id
+        self._name = data[Team._structure_team[0]]
+        self._bd_players = bd_players
+        self._players = self.players
+
+    @property
+    def players(self):
+        consist = {}
+        for player in self._bd_players.obj_in_dict:
+            if self._bd_players.obj_in_dict[player].team_id == self._team_id:
+                consist[player] = self._bd_players.obj_in_dict[player]
+        return consist
 
 
 class Player:
-    def __init__(self, name, team_id):
-        self.player_id = uuid.uuid4()
-        self.name = name
-        self.team_id = team_id
+    _structure_player = ('Name', 'Family', 'Team_id')
 
-    #@property
-    #def team(self):
+    def __init__(self, player_id, data):
+        self._player_id = player_id
+        self.name = data[Player._structure_player[0]]
+        self.family = data[Player._structure_player[1]]
+        self.team_id = data[Player._structure_player[2]]
 
 
 class Match:
-    def __init__(self, date, location, result, team1, team2, players):
-        self.match_id = uuid.uuid4()
-        self.date = date
-        self.location = location
-        self.result = result
-        self.team1 = team1
-        self.team2 = team2
-        self.players = players
+    def __init__(self, data):
+        self.match_id = data[0]
+        self.date = data[1]
+        self.location = data[2]
+        self.result = data[3]
+        self.team1 = data[4]
+        self.team2 = data[5]
+        self.players = data[6]
 
 
 if __name__ == "__main__":
-    pass
+    players = BD('players.json', 'players')
+    players.bd_initializations()
+    teams = BD('teams.json', 'teams')
+    teams.bd_initializations()
+
